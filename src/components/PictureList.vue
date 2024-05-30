555820f43afa9344919bfd2be20f8b096f552823
mojiez <937691466@qq.com>
2024-05-30 16:59:14 +0800

    前端整合pictureList，实现对图片用户post的text搜索和展示

--- src/components/PictureList.vue
+++ src/components/PictureList.vue
@@ -1,10 +1,34 @@
 <template>
   图片列表
-  <img :src="avator" />
+  <img :src="logo" />
+  <a-list
+    item-layout="horizontal"
+    :grid="{ gutter: 16, column: 4 }"
+    :data-source="props.pictureList"
+  >
+    <template #renderItem="{ item }">
+      <a-card hoverable>
+        <template #cover>
+          <img alt="example" :src="item.url" />
+        </template>
+        <a-card-meta :title="item.title"></a-card-meta>
+      </a-card>
+    </template>
+  </a-list>
 </template>
 
 <script setup lang="ts">
+import logo from "@/assets/logo.png";
+import { withDefaults, defineProps, toRefs } from "vue";
 import avator from "@/assets/avator.jpeg";
+// todo 这一段是什么意思
+interface Props {
+  pictureList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  pictureList: () => [],
+});
 </script>
 
 <style scoped></style>
--- src/pages/indexPage.vue
+++ src/pages/indexPage.vue
@@ -8,8 +8,6 @@
       size="large"
       @search="onSearch"
     />
-    {{ JSON.stringify(searchParams) }}
-    {{ JSON.stringify(postList) }}
     <MyDivider />
     <a-tabs v-model:activeKey="activeKey" @change="onTabChange">
       <a-tab-pane key="post" tab="文章">
@@ -19,7 +17,7 @@
         <UserList :user-list="userList" />
       </a-tab-pane>
       <a-tab-pane key="picture" tab="图片">
-        <PictureList />
+        <PictureList :picture-list="pictureList" />
       </a-tab-pane>
     </a-tabs>
   </div>
@@ -35,18 +33,6 @@ import router from "@/router";
 import { useRoute } from "vue-router";
 import myAxios from "@/plugins/myAxios";
 
-myAxios.post("post/list/page/vo", {}).then((res: any) => {
-  postList.value = res.records;
-});
-
-myAxios.post("user/list/page/vo", {}).then((res: any) => {
-  userList.value = res.records;
-});
-
-const searchText = ref("");
-const route = useRoute();
-const activeKey = route.params.category;
-
 const initSearchParams = {
   text: "",
   pageSize: 10,
@@ -55,20 +41,106 @@ const initSearchParams = {
 const searchParams = ref(initSearchParams);
 const postList = ref([]);
 const userList = ref([]);
+const pictureList = ref([]);
+
+myAxios.post("post/list/page/vo", {}).then((res: any) => {
+  postList.value = res.records;
+});
+myAxios
+  .post("picture/list/page/vo", {
+    current: 5,
+    pageSize: 10,
+    searchText: "",
+    sortField: "",
+    sortOrder: "",
+  })
+  .then((res: any) => {
+    pictureList.value = res.records;
+  });
+myAxios.post("user/list/page/vo", {}).then((res: any) => {
+  userList.value = res.records;
+});
+
+const searchText = ref("");
+const route = useRoute();
+const activeKey = route.params.category;
+
 const onSearch = (value: string) => {
-  alert(value);
+  // 每次执行onSearch 就会改变页面的路由
+  console.log(value);
   router.push({
     query: searchParams.value,
   });
+  loadData(searchParams.value);
 };
 
 watchEffect(() => {
+  // 这里是为了刷新页面的时候 searchParams的值不丢失
+  // 用url控制页面
   searchParams.value = {
     ...initSearchParams,
     text: route.query.text,
   } as any;
 });
 
+/**
+ * 加载数据
+ * @param params
+ */
+const loadData = (params: any) => {
+  const postQuery = {
+    ...params,
+    searchText: params.text,
+  };
+  myAxios.post("post/list/page/vo", postQuery).then((res: any) => {
+    postList.value = res.records;
+  });
+
+  const pictureQuery = {
+    ...params,
+    searchText: params.text,
+  };
+  myAxios.post("picture/list/page/vo", pictureQuery).then((res: any) => {
+    pictureList.value = res.records;
+  });
+  const userQuery = {
+    ...params,
+    userName: params.text,
+  };
+  myAxios.post("user/list/page/vo", userQuery).then((res: any) => {
+    userList.value = res.records;
+  });
+};
+// watchEffect(() => {
+//   myAxios.post("post/list/page/vo", {}).then((res: any) => {
+//     postList.value = res.records;
+//   });
+//
+//   // 这里相当于监听了 searchParams.value.text,
+//   myAxios
+//     .post("picture/list/page/vo", {
+//       current: 5,
+//       pageSize: 10,
+//       searchText: searchParams.value.text,
+//       sortField: "",
+//       sortOrder: "",
+//     })
+//     .then((res: any) => {
+//       pictureList.value = res.records;
+//     });
+//   myAxios.post("user/list/page/vo", {}).then((res: any) => {
+//     userList.value = res.records;
+//   });
+// });
+
+// 首次请求
+// todo 目前是在页面加载时，调用三个接口分别获取文章、图片、用户数据
+
+// 两种不同的业务场景
+// 其实可以用户点某个tab的时候，只调用这个tab的接口 有可能还要查询一些其他的信息，比如其他数据的总数
+// 如果是针对聚合内容的网页， 其实可以直接一个请求搞定
+
+loadData(searchParams.value);
 const onTabChange = (key: string) => {
   router.push({
     path: `/${key}`,
